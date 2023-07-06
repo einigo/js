fa6954efd6acaebdcea6c07c71e9fdb3c148bb11
Enzo Iñigo <enzoinigo96@gmail.com>
2023-07-06 16:24:20 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,18 @@
+"use strict"
+
+console.log("Hola mundo");
+
+let usuario = "pepe";
+usuario = "juan";
+
+usuario = "Pedro"
+
+console.log(usuario)
+
+let respuestaUsuario
+
+//respuestaUsuario = prompt("¿cual es tu nombre?")
+
+console.log (respuestaUsuario)
+
+document.write("hola")
